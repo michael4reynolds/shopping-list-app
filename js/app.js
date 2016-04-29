61886ae8c17d227d4db35e5dc5741756990d8cd1
Michael Reynolds <michael4reynolds@gmail.com>
2016-04-29 12:31:20 -0400

    item goes into 'Needed' section

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('#newitem').on('keydown', function (e) {
+    if (e.keyCode === 13) {
+      var item = $(this).val();
+      $('.needed ul').append('\
+        <li class="itemrow">\
+          <input type="checkbox" name="item_a">\
+          <input type="text" name="item_b" value="' + item + '" class="item">\
+        </li>');
+      e.preventDefault();
+      $(this).val('');
+    }
+  });
+});
