ae53244dcd9b79c8bb20498cd3bf55f26f648b59
Michael Reynolds <michael4reynolds@gmail.com>
2016-04-30 07:34:57 -0400

    use esnext

--- js/app.js
+++ js/app.js
@@ -10,27 +10,23 @@ $(document).ready(function () {
       e.preventDefault()
       $(this).val('')
     }
-  });
+  })
 
   $('.needed input[type=checkbox]').on('click', function () {
-    console.log($(this))
-    console.log($(this).parent())
-
-    $(this).next().toggleClass('checked');
-  });
+    $(this).next().toggleClass('checked')
+  })
 
   $('input.add').on('click', function () {
-    console.log($(this))
-    $('.item.checked').each(function(){
-      var html = "<li class='itemrow'>";
-      html += "<input type='image' src='images/refresh.png' alt='Buy again'>";
-      html += "<input type='text' value='"+$(this).val()+"' class='item'>";
-      html += "</li>";
-      $('.purchased ul').prepend(html);
-      $(this).parent().remove();
-    });
-    console.log(checked.length)
-  });
+    $('.item.checked').each(function () {
+      var html = `
+        <li class='itemrow'>
+          <input type='image' src='images/refresh.png' alt='Buy again'>
+          <input type='text' value='${$(this).val()}' class='item'>
+        </li>`
+      $('.purchased ul').prepend(html)
+      $(this).parent().remove()
+    })
+  })
 
 
 })
